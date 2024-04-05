64ef8229ea8561fda48db5b068a5abd45d74051b
SyakirZaidi <zsyakir20@gmail.com>
2024-04-05 09:13:44 +0800

    day 6/366

---
+++ 6-doubleInteger.js
@@ -0,0 +1,12 @@
+// WPU Coding Challenge 2024
+// Day - 6/366 (You Can't Code Under Pressure #1)
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+// function doubleInteger(i) {
+//   // i will be an integer. Double it and return it.
+//   return i * 2;
+// }
+
+const doubleInteger = (i) => i * 2;
+
+console.log(doubleInteger(8));
