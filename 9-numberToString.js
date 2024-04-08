388c12fe9ce0fecc1faf27dc674bb97b31797f74
SyakirZaidi <zsyakir20@gmail.com>
2024-04-08 16:36:36 +0800

    day 9/366

---
+++ 9-numberToString.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// Day - 9/366 (Convert a Number to a String!)
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// function numberToString(num) {
+//   return `${num}`;
+// }
+
+const numberToString = (num) => `${num}`;
+
+console.log(numberToString(678));
