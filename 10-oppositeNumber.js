46ed34ccc6781ed1fdec62791c0006d911489d00
SyakirZaidi <zsyakir20@gmail.com>
2024-04-25 08:30:15 +0800

    day 10/366

---
+++ 10-oppositeNumber.js
@@ -0,0 +1,11 @@
+// WPU Coding Challenge 2024
+// Day - 910/366 (Opposite number)
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+// function opposite(number) {
+//     return number - number * 2;
+// }
+
+const opposite = (number) => number - number * 2;
+
+console.log(opposite(9));
