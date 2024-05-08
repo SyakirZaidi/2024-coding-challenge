3e7ba9ccbf126b838173f8fdbcbb8dfb241862f1
SyakirZaidi <zsyakir20@gmail.com>
2024-05-08 16:02:26 +0800

    day 12

---
+++ 12-squareSum.js
@@ -0,0 +1,16 @@
+// WPU Coding Challenge 2024
+// Day - 12/366 (Square(n) Sum)
+// https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+// function squareSum(numbers) {
+//   if (numbers == 0) {
+//     return 0;
+//   } else {
+//     return numbers.reduce((a, c) => a + Math.pow(c, 2), 0);
+//   }
+// }
+
+const squareSum = (numbers) =>
+  numbers == 0 ? 0 : numbers.reduce((sum, n) => sum + Math.pow(n, 2), 0);
+
+console.log(squareSum([6, 12, -1, -2, 12, -8, -9, -5]));
